6a4aa687d4d0a50ae028a192a236de33daf3c297
nishaodong <416363428@qq.com>
2024-12-15 21:16:57 +0800

    nft

---
+++ contracts/ReuseMemory.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+contract Called {
+    function add(uint256 a, uint256 b) external pure returns(uint256) {
+        return a + b;
+    }
+    function add1(uint256 a, uint256 b) external pure returns(uint256) {
+        require(a>0&&b>0);
+        return a + b;
+    }
+    function add2(uint256 a, uint256 b) external pure returns(uint256) {
+        require(a>0);
+        require(b>0);
+        return a + b;
+    }
+}
+
+
+contract Solidity {
+    // cost: 7262
+    function call(address calledAddress) external pure returns(uint256) {
+        Called called = Called(calledAddress);
+        uint256 res1 = called.add(1, 2);
+        uint256 res2 = called.add(3, 4);
+
+        uint256 res = res1 + res2;
+        return res;
+    }
+}
+
+
+contract Assembly {
+    // cost: 5281
+    function call(address calledAddress) external view returns(uint256) {
+        assembly {
+            // check that calledAddress has code deployed to it
+            if iszero(extcodesize(calledAddress)) {
+                revert(0x00, 0x00)
+            }
+
+            // first call
+            mstore(0x00, hex"771602f7")
+            mstore(0x04, 0x01)
+            mstore(0x24, 0x02)
+            let success := staticcall(gas(), calledAddress, 0x00, 0x44, 0x60, 0x20)
+            if iszero(success) {
+                revert(0x00, 0x00)
+            }
+            let res1 := mload(0x60)
+
+            // second call
+            mstore(0x04, 0x03)
+            mstore(0x24, 0x4)
+            success := staticcall(gas(), calledAddress, 0x00, 0x44, 0x60, 0x20)
+            if iszero(success) {
+                revert(0x00, 0x00)
+            }
+            let res2 := mload(0x60)
+
+            // add results
+            let res := add(res1, res2)
+
+            // return data
+            mstore(0x60, res)
+            return(0x60, 0x20)
+        }
+    }
+}
